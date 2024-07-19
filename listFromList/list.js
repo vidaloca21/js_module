7b039e4c09cbe9a9d4e0a6b3aa7724640e3552aa
DESKTOP-CI <jihoon>
2024-07-19 16:12:39 +0900

    ??234

---
+++ listFromList/list.js
@@ -0,0 +1,10 @@
+const codes = ["005930", "443060", "078020", "000660"];
+const codeList = {
+  "005930": { val: "005930" },
+  "035420": { val: "035420" },
+  "035720": { val: "035720" },
+  "000660": { val: "000660" },
+  443060: { val: "443060" },
+  207940: { val: "207940" },
+};
+document.addEventListener("DOMContentLoaded", function () {});
